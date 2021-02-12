c7de3a5af18f678fe0e08e948a81819d6ec8ec70
jordy muniz <jordy.muniz96@gmail.com>
2021-02-11 19:40:06 -0600

    added gitignore

---
+++ src/main/java/com/datingapp/datemates/DateMatesApplication.java
@@ -0,0 +1,13 @@
+package com.datingapp.datemates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateMatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateMatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datingapp/datemates/DateMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datingapp.datemates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateMatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
